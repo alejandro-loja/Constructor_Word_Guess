f8b5b02981fe667233d4ba14f616514b113c20aa
Alejandro Loja <alejandro.loja@gmail.com>
2018-11-14 22:20:51 -0500

    letter.js works

--- Letter.js
+++ Letter.js
@@ -1,1 +1,34 @@
 console.log('Letter.js. - Loaded')
+
+function Letter(oneLetter){
+    this.oneLetter = oneLetter;
+    this.guessedOrNot = false;
+    this.returnOneLetter = function (){
+        if (this.guessedOrNot){
+            return this.oneLetter;
+        }
+        else {
+            return " _ ";
+        }
+    };
+    this.isLetter = function (guess) {
+        if (guess === this.oneLetter){
+            this.guessedOrNot = true;
+            return "correct!";
+        }
+        else {
+            return "Wrong Letter.";
+        }
+    }
+}
+
+// Testing
+
+// var letterR = new Letter("r");
+// console.log(letterR.returnOneLetter());
+// console.log(letterR.isLetter("R"));
+// console.log(letterR.returnOneLetter());
+// console.log(letterR.isLetter("r"));
+// console.log(letterR.returnOneLetter());
+
+module.exports = Letter;
--- Word.js
+++ Word.js
@@ -1,4 +1,25 @@
-var letter = require('./Letter.js');
+var Letter = require('./Letter.js');
+console.log('Word.js. - Loaded');
 
+function Word(word) {
+    this.word = word.split("");
+    this.wordObjArray = [];
+    this.arrayOfLettersObj = this.word.forEach(function (oneLetter){
+        // this.wordObjArray.push("oneLetter");
+    });
+    this.letterToWords = function () {
+        var createdWord = this.word.join(" ");
+        return createdWord;
+    }
+    this.isLetterInWord = function (character) {
 
-console.log('Word.js. - Loaded')
+    }
+    this.printStats = function () {
+        console.log(this.word);
+        console.log(this.wordObjArray);
+    }
+}
+
+var hi = new Word('Banana');
+hi.printStats();
+console.log(hi.letterToWords());
